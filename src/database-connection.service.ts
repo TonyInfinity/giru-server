19e29c3a982bc06d3f66b17a0dd9519d87018807
TonyInfinity <quachhengtony@gmail.com>
2021-11-18 21:29:14 +0700

    Initialize project, create and connect to database

---
+++ src/database-connection.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+@Injectable()
+export class DatabaseConnectionService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ['dist/**/*.entity.js'],
+      synchronize: true,
+      dropSchema: false,
+      logging: true,
+    };
+  }
+}
