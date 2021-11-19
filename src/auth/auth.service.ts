9c823c557f12b71e4bf43be075375dec48efc641
TonyInfinity <quachhengtony@gmail.com>
2021-11-19 10:08:50 +0700

    Add jwt authentication

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,59 @@
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/entities/user.entity';
+import { LoginDTO, RegisterDTO } from 'src/models/user.model';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(UserEntity) private userRepo: Repository<UserEntity>,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(credentials: RegisterDTO) {
+    try {
+      const user = this.userRepo.create(credentials);
+      await user.save();
+      const payload = { username: user.username };
+      const token = this.jwtService.sign(payload);
+      return {
+        user: {
+          ...user.toJSON(),
+          token,
+        },
+      };
+    } catch (err) {
+      if (err.code === '23505') {
+        throw new ConflictException('Username has already been taken.');
+      }
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async login({ email, password }: LoginDTO) {
+    try {
+      const user = await this.userRepo.findOne({ where: { email } });
+      const isValid = await user.comparePassword(password);
+      if (!isValid) {
+        throw new UnauthorizedException('Invalid credentials.');
+      }
+      const payload = { username: user.username };
+      const token = this.jwtService.sign(payload);
+      return {
+        user: {
+          ...user.toJSON(),
+          token,
+        },
+      };
+    } catch (err) {
+      throw new UnauthorizedException('Invalid credentials.');
+    }
+  }
+}
